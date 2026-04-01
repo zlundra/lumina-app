5afd6c61ec02c78ec4fc11d66d6b85c3dcc5f77f
Tina <50179199+zlundra@users.noreply.github.com>
2026-04-01 20:54:11 +0100

    fix: restore working base path

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/',
-  build: {
-    rollupOptions: {
-      input: {
-        app: 'app.html',
-      }
-    }
-  }
+  base: '/',
 })
